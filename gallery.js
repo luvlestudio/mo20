8c02152f5abfd902367a60e1e04d2c22df062050
luvlestudio <sowoojoo31@gmail.com>
2024-09-04 13:24:45 +0900

    모청제작

--- gallery.js
+++ gallery.js
@@ -46,9 +46,9 @@ window.onload = () => {
     Kakao.Share.sendDefault({
       objectType: "feed",
       content: {
-        title: "김준영 ♥ 박민주의 모바일 청첩장",
+        title: "권태현 ♥ 김보아의 모바일 청첩장",
         description: "결혼식에 초대합니다♡",
-        imageUrl: "https://i.imgur.com/tYuIfbQ.png",
+        imageUrl: "https://i.imgur.com/1ZxFpdq.png",
         imageWidth: 1200,
         imageHeight: 630,
         link: {
